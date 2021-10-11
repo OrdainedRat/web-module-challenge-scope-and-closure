1c1d911e023c72ecdd98786a83b897c881eef056
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2021-10-11 15:31:52 -0500

    finished product

--- index.js
+++ index.js
@@ -29,10 +29,19 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   1. What is the difference between counter1 and counter2?
   
+    Counter1 uses a declaration and a callback function with variables declared in the high-order function. Counter2 uses a global variable and one function. 
+
+  
   2. Which of the two uses a closure? How can you tell?
+
+    Both. Neither of two functions that operate on the count variable contain it.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+    Counter1 would be preferable in scenarios that require the reuse of the count name.
+    Counter2 would be preferable for simplicity in scenarios that don't require reuse of the count name.
+
 */
 
 // counter1 code
@@ -62,10 +71,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * 3);
 }
 
+console.log('task 2', inning());
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -81,17 +91,28 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningcb, number){
+  let score = new Object();
+  for(let i = 0; i < number; i++) {
+  score.Home = inning();
+  score.Home += inning();
+  score.Away = inning();
+  score.Away += inning();
 }
+  return score;
+}
+console.log('task 3', finalScore(inning(), 9))
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningcb) {
+  let score = new Object();
+  score.Home = inning();
+  score.Away = inning();
+  return score;
 }
 
 
